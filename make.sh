2d9a6cc9a00938b7d32e953df974d98405150168
Duong Bao Duy <baoduy.duong0206@gmail.com>
2015-05-12 16:35:18 +0700

    - fix make.sh error when finished.
    Signed-off-by: Duong Bao Duy <baoduy.duong0206@gmail.com>

--- make.sh
+++ make.sh
@@ -12,7 +12,7 @@
 
 rm -rf submodule
 git clone https://gitlab.com/baoduyduong0206/dbd-tools.git submodule
-cd dbd-tools 
+cd submodule
 for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master`; do
     git branch --track ${branch##*/} $branch
 done
@@ -22,4 +22,4 @@ git gc --aggressive
 git prune
 git remote rm origin
 git remote add origin git@github.com:duongbaoduy/dotemacs.git
-read -p "created ..."
+exit 0
